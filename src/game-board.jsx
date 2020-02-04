8ccb59527d02a46110a2227b56e20ec32bc3d339
Alex Aranda <alexraranda@gmail.com>
2020-02-04 12:22:00 -0800

    cards layed out, used media query to make responsiev for mobile and desktop

--- src/game-board.jsx
+++ src/game-board.jsx
@@ -1,7 +1,57 @@
 import React from 'react'
 
-function GameBoard(){
-  return <div className="gameBoard">Test GameBoard</div>
+class GameBoard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cards: [],
+      firstCardToCheck: null,
+      secondCardTocheck: null
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ cards: ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "black", "white"] })
+  }
+
+  shuffleDeck(arrayOfCards){
+    let currentIndex = arrayOfCards.length, temp, randomIndex;
+    while(currentIndex !== 0){
+      randomIndex = Math.floor(Math.random()*currentIndex);
+      currentIndex--;
+      temp = arrayOfCards[currentIndex];
+      arrayOfCards[currentIndex] = arrayOfCards[randomIndex];
+      arrayOfCards[randomIndex] = temp;
+    }
+    return arrayOfCards;
+  }
+
+  layoutCards() {
+    if (this.state.cards.length) {
+      let deckArray = this.state.cards.concat(this.state.cards)
+      deckArray = this.shuffleDeck(deckArray)
+      deckArray = deckArray.map(cardItem)
+      return (
+        <div className="gameBoard container">
+          {deckArray}
+        </div>
+      )
+    } else {
+      return <h1>WAITING TO LOAD</h1>
+    }
+  }
+
+  render() {
+    return this.layoutCards()
+  }
 }
 
 export default GameBoard
+
+function cardItem(cardClass, index) {
+  return (
+    <div className="cardBack" key={index}>
+      <div className={`${cardClass} hidden cardFront`} />
+    </div>
+  )
+}
